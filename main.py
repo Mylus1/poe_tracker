68b4a3fb10986b81b5d099a94c8e65f564dff81e
Mylus1 <bilal.leadbitter@gmail.com>
2026-05-18 22:15:48 +0100

    Test commit

--- main.py
+++ main.py
@@ -9,6 +9,5 @@ response = requests.get(url)
 response.raise_for_status()  # Check if the request was successful
 data = response.json()
 
-
 for currency in data.get("lines", []):
     print(f"{currency['id']}: {currency['primaryValue']} chaos")
